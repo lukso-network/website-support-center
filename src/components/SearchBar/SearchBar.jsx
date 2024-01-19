f022c343a675af074a6d3abdf2f24549ce38a41d
Hugo Masclet <hugo@lukso.io>
2024-01-19 13:39:37 +0100

    Add search bar on homepage

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import OriginalSearchBar from '@theme-original/SearchBar';
+
+import React from 'react';
+
+export default function SearchBarWithAnalytics(props) {
+  return (
+    <div>
+      <OriginalSearchBar {...props} />
+    </div>
+  );
+}
